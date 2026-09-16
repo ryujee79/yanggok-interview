fad5bc0f63ccc0d85a4c48e255b0afcc2d155dc5
ryujee79 <ryujee79@yanggok.hs.kr>
2026-09-16 16:51:27 +0900

    Connect GitHub Pages to Apps Script backend

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.YANGGOK_CONFIG = {
-  apiUrl: '',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbxXgQRONzfjpwbat_cpQSfoavWI6CKkwf2LbsnkF325rhZ_EDJYdLKaQ4kUgyoN3wN4Gg/exec',
   siteName: '2027 제시문 면접 지도',
   pollMs: 8000
 };
